95a338b3f38a0db6e3d3d16fb8806179d8ef687c
sadiaism <ismailmoiz80@gmail.com>
2026-01-24 21:22:00 +0500

    THE II

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Important: Enable path aliases for both server and client builds
+    config.resolve.modules.push(__dirname);
+    return config;
+  },
+};
 
 module.exports = nextConfig;
